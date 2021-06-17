8f8612e6f1632bd09dad854b9e85553d1ee517f4
bbane <ssj7410@gmail.com>
2021-06-17 15:34:46 +0900

    Ajax

--- src/main/java/com/jun/blog/controller/api/UserApiController.java
+++ src/main/java/com/jun/blog/controller/api/UserApiController.java
@@ -2,12 +2,17 @@ package com.jun.blog.controller.api;
 
 import com.jun.blog.dto.ResponseDto;
 import com.jun.blog.model.User;
+import com.jun.blog.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserApiController {
+
+    @Autowired
+    private UserService userService;
 
     @PostMapping("/api/user")
     public ResponseDto<Integer> save(@RequestBody User user){
--- src/main/java/com/jun/blog/service/UserService.java
+++ src/main/java/com/jun/blog/service/UserService.java
@@ -5,12 +5,16 @@ import com.jun.blog.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
+
+// 스프링이 컴포넌트 스캔을 통해서 Bean에 등록을 해줌. IoC를 해준다
 @Service
 public class UserService {
 
     @Autowired
     private UserRepository userRepository;
 
+    @Transactional
     public int 회원가입(User user){
         try {
             userRepository.save(user);
