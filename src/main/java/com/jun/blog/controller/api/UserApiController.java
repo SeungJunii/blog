3fbe5f1bb1b713e2f15b77e36974516791d2899d
bbane <ssj7410@gmail.com>
2021-06-18 20:53:50 +0900

    Log IN

--- src/main/java/com/jun/blog/controller/api/UserApiController.java
+++ src/main/java/com/jun/blog/controller/api/UserApiController.java
@@ -10,11 +10,16 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpSession;
+
 @RestController
 public class UserApiController {
 
     @Autowired
     private UserService userService;
+
+    @Autowired
+    private HttpSession session;
 
     @PostMapping("/api/user")
     public ResponseDto<Integer> save(@RequestBody User user){//username,password,email
@@ -23,5 +28,17 @@ public class UserApiController {
         userService.회원가입(user);
         return new ResponseDto<Integer>(HttpStatus.OK.value(),1);
         //자바오브젝트를 JSON으로 변환하여 리턴
+    }
+
+    //
+    @PostMapping("/api/user/login")
+//    public ResponseDto<Integer> login(@RequestBody User user, HttpSession session){
+    public ResponseDto<Integer> login(@RequestBody User user){
+        System.out.println("UserApiController : login 호출됨");
+        User principal = userService.로그인(user); //principal(접근주체)
+        if (principal !=null){
+            session.setAttribute("principal",principal);
+        }
+        return new ResponseDto<Integer>(HttpStatus.OK.value(),1);
     }
 }
